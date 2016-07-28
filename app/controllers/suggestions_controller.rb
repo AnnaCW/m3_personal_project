ea7498670b6ec5fba12908710bd6e8655c629756
Anna Weisbrodt <aweisbro@gmail.com>
2016-07-27 21:31:35 -0600

    fix default selection

--- app/controllers/suggestions_controller.rb
+++ app/controllers/suggestions_controller.rb
@@ -4,10 +4,14 @@ class SuggestionsController < ApplicationController
     user = current_user if current_user
     @genres = Suggestion.genres(user)
 
-    if params[:commit]
-      selected_genre = params[:genre]
-      @genre = selected_genre.capitalize
-      @suggestions_from_genre = Suggestion.get_from_seeds(user, {"seed_artists" => nil, "seed_tracks" => nil, "seed_genres" => selected_genre} )
+    if params[:commit] && params[:genre]
+      if params[:genre] == ""
+        @suggestions_from_genre == false
+      else
+        selected_genre = params[:genre]
+        @genre = selected_genre.capitalize
+        @suggestions_from_genre = Suggestion.get_from_seeds(user, {"seed_artists" => nil, "seed_tracks" => nil, "seed_genres" => selected_genre} )
+      end
     end
   end
 
