1d302f03d7bfdc295bf4c5e3df2964b079fe0366
Anna Weisbrodt <aweisbro@gmail.com>
2016-07-27 21:37:25 -0600

    start recommendations by decade

--- app/controllers/suggestions_controller.rb
+++ app/controllers/suggestions_controller.rb
@@ -3,6 +3,7 @@ class SuggestionsController < ApplicationController
   def index
     user = current_user if current_user
     @genres = Suggestion.genres(user)
+    @decades = Suggestion.decades(user)
 
     if params[:commit] && params[:genre]
       if params[:genre] == ""
@@ -11,6 +12,15 @@ class SuggestionsController < ApplicationController
         selected_genre = params[:genre]
         @genre = selected_genre.capitalize
         @suggestions_from_genre = Suggestion.get_from_seeds(user, {"seed_artists" => nil, "seed_tracks" => nil, "seed_genres" => selected_genre} )
+      end
+
+    elsif params[:commit] && params[:decade]
+      if params[:decade] == ""
+        @suggestions_from_decade == false
+      else
+        selected_genre = params[:decade]
+        @decade = selected_genre.capitalize
+        @suggestions_from_decade = Suggestion.get_from_seeds(user, {"seed_artists" => nil, "seed_tracks" => nil, "seed_genres" => selected_genre} )
       end
     end
   end
