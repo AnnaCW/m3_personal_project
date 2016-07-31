bebae0ca37a77d22afdb71204b57a62c52cd7c1d
Anna Weisbrodt <aweisbro@gmail.com>
2016-07-31 06:45:40 -0600

    refactor into suggestions presenter

--- app/presenters/suggestions_presenter.rb
+++ app/presenters/suggestions_presenter.rb
@@ -13,11 +13,46 @@ class SuggestionsPresenter
     @decades ||= Suggestion.decades
   end
 
-  
+  def suggestions_from_genre
+    if params[:commit] && params[:genre]
+      if params[:genre] == ""
+        @suggestions_from_genre == false
+      else
+        selected_genre = params[:genre]
+        @suggestions_from_genre = Suggestion.get_from_seeds(user, {"seed_artists" => nil, "seed_tracks" => nil, "seed_genres" => selected_genre} )
+      en
+    end
+  end
+
+  def genre
+    @genre = params[:genre].capitalize
+  end
+
+
+    def suggestions_from_decade
+      if params[:commit] && params[:decade]
+        if params[:decade] == ""
+          @suggestions_from_decade == false
+        else
+          selected_decade = params[:decade]
+          @suggestions_from_decade = Suggestion.decade_playlists(user, selected_decade)
+        end
+      end
+   end
+
+   def decade
+     @decade = params[:decade]
+   end
+
+
 private
 
   def user
     @_user
+  end
+
+  def params
+    @_params
   end
 
 end
