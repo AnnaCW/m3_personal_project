edc6d4cd1cbf4c4718cc19dea8e57cbc870cee6f
Anna Weisbrodt <aweisbro@gmail.com>
2016-07-31 09:37:26 -0600

    basic spec for admin features passing

--- spec/features/admin_views_listening_sessions_spec.rb
+++ spec/features/admin_views_listening_sessions_spec.rb
@@ -6,6 +6,9 @@ feature "admin views all listening sessions" do
       user = create(:user, role: 1)
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return( user )
 
+      visit admin_listening_sessions_path
+
+      expect(page).to have_content("Export CSV")
     end
   end
 end
