e79c2a3785e9f1289f2105317daf338f959c5162
Anna Weisbrodt <aweisbro@gmail.com>
2016-08-01 05:47:35 -0600

    add permissions service

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   helper_method :current_user
-  # before_action :authorize!
+  before_action :authorize!
 
   def current_user
     @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
@@ -12,11 +12,20 @@ class ApplicationController < ActionController::Base
 
   private
 
-  # def authorize!
-  #   redirect_to(root_path, warning: "You Do Not Have Access") unless current_admin?
-  # end
-
   def current_admin?
     current_user && current_user.admin?
   end
+
+  def authorize!
+    redirect_to(root_url, warning: "You Do Not Have Access") unless authorized?
+  end
+
+  def authorized?
+    current_permission.allow?
+  end
+
+  def current_permission
+    @current_permission ||= PermissionsService.new(current_user, params[:controller], params[:action])
+  end
+
 end
