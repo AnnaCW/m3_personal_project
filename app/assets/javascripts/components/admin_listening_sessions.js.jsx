102811b2c507a6bc95e33aa0705174aba4eb0d2c
Anna Weisbrodt <aweisbro@gmail.com>
2016-09-11 19:22:34 -0600

    add react component for admin ls

---
+++ app/assets/javascripts/components/admin_listening_sessions.js.jsx
@@ -0,0 +1,37 @@
+class AdminListeningSessions extends React.Component {
+  constructor(props) {
+    super();
+    this.state = {
+      listeningSessions: props.listeningSessions
+    };
+  }
+
+  render()  {
+    var listeningSessions = this.state.listeningSessions.map( (listeningSession) => {
+      console.log(listeningSession)
+      return <ListeningSession key={listeningSession.id} listeningSession={listeningSession} />
+    })
+    return (
+      <table className="table listening-sessions">
+        <thead>
+          <tr>
+            <td>Session Name</td>
+            <td>User ID</td>
+            <td>Date Updated</td>
+            <td>Date Created</td>
+            <td>Item Type</td>
+            <td>Item ID</td>
+            <td>Agitation Before</td>
+            <td>Agitation After</td>
+            <td>Awareness Before</td>
+            <td>Awareness After</td>
+            <td>Communicativeness Before</td>
+            <td>Communicativeness After</td>
+            <td>Duration</td>
+          </tr>
+        </thead>
+        <tbody>{listeningSessions}</tbody>
+      </table>
+    )
+  }
+}
